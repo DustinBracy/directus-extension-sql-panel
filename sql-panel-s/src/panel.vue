d9942f67e13ceee3602bf4393b9295c430d96523
Stathis90 <90stathis@gmail.com>
2023-07-26 13:13:29 +0300

    fix sorting

--- sql-panel-s/src/panel.vue
+++ sql-panel-s/src/panel.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="sql-panel-container" :class="{ 'has-header': showHeader }">
 		<v-table class="table" :showResize="true" v-model:headers="headers" :items="items" :loading="loading"
-			@update:sort="onSortUpdate">
+			@update:sort="onSortUpdate" v-model:sort>
 			<template v-for="(header, rowIndex) in headers" v-slot:[`item.${header.value}`]="{ item }">
 				<div :key="rowIndex + 'link'" v-if="shouldRenderLink(item[header.value])"
 					@click="openLink(item[header.value])" class="custom-link">
@@ -34,8 +34,8 @@ export default {
 			items: [],
 			loading: true,
 			sortConfig: {
-				by: "",
-				desc: true,
+				by:"",
+				desc:"false"
 			},
 		};
 	},
@@ -65,39 +65,19 @@ export default {
 		onSortUpdate(sortEvent) {
 			console.log(sortEvent)
 			if (sortEvent !== null) {
-				// Check if the same column is clicked again
-				const isSameColumn = this.sortConfig.by === sortEvent.by;
+				this.items.sort((a, b) => {
+					const valueA = a[sortEvent.by];
+					const valueB = b[sortEvent.by];
 
-				// Toggle the sort direction for the same column
-				if (isSameColumn) {
-					this.sortConfig.desc = !this.sortConfig.desc;
-				} else {
-					// If a different column is clicked, use the default sort direction (ascending)
-					this.sortConfig.by = sortEvent.by;
-					this.sortConfig.desc = false;
-				}
+					// If the values are strings, use localeCompare for string comparison
+					if (typeof valueA === "string" && typeof valueB === "string") {
+						return sortEvent.desc ? valueB.localeCompare(valueA) : valueA.localeCompare(valueB);
+					}
 
-				// Perform the sorting based on the updated sort configuration
-				this.sortItems();
+					// For numerical values, use normal comparison
+					return sortEvent.desc ? valueB - valueA : valueA - valueB;
+				});
 			}
-		},
-		sortItems(sortEvent) {
-			const sortBy = this.sortConfig.by;
-			const sortDesc = this.sortConfig.desc;
-
-			// Perform the sorting logic here
-			this.items.sort((a, b) => {
-				const valueA = a[sortBy];
-				const valueB = b[sortBy];
-
-				// If the values are strings, use localeCompare for string comparison
-				if (typeof valueA === "string" && typeof valueB === "string") {
-					return sortDesc ? valueB.localeCompare(valueA) : valueA.localeCompare(valueB);
-				}
-
-				// For numerical values, use normal comparison
-				return sortDesc ? valueB - valueA : valueA - valueB;
-			});
 		},
 	},
 };
