fe2700f24acda91b0ffb1c1140f3e82d082bdbe3
Dustin Bracy <dustin.bracy@flmarine.com>
2023-09-26 13:39:20 -0500

    remove square brackets

--- sql-panel-s-endpoint/src/index.js
+++ sql-panel-s-endpoint/src/index.js
@@ -1,14 +1,19 @@
 "use strict";
 
-const endpointHandler = function (router, { services: directusServices, database }) {
+const endpointHandler = function (
+  router,
+  { services: directusServices, database }
+) {
   router.get("/:id", async function (req, res) {
     try {
       // Get the ID parameter from the request
       const panelId = req.params.id;
 
       // Fetch the panel options from the 'directus_panels' table using the provided ID
-      const panelQueryResult = await database.raw(`SELECT [options] FROM [directus_panels] WHERE [id] = '${[panelId]}'`);
-      
+      const panelQueryResult = await database.raw(
+        `SELECT options FROM directus_panels WHERE id = '${panelId}'`
+      );
+
       // Parse the options JSON data into an object
       const parsedOptions = JSON.parse(panelQueryResult[0].options);
 
@@ -16,17 +21,22 @@ const endpointHandler = function (router, { services: directusServices, database
       const queryResult = await database.raw(parsedOptions.sql);
 
       // Extract the column headers from the query result
-      const headers = Object.keys(queryResult[0]).map(column => ({ text: column, value: column }));
+      const headers = Object.keys(queryResult[0]).map((column) => ({
+        text: column,
+        value: column,
+      }));
 
       // Respond with the formatted data as JSON
       res.json({
         headers: headers,
-        items: queryResult
+        items: queryResult,
       });
     } catch (error) {
       // If an error occurs, log it and respond with an appropriate error message
       console.error("Error in endpoint:", error);
-      res.status(500).json({ error: "An error occurred while processing the request." });
+      res
+        .status(500)
+        .json({ error: "An error occurred while processing the request." });
     }
   });
 };
